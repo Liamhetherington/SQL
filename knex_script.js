b88b2fe9e857f2a161eec55a379587bf240a469c
Liam Hetherington <liambhetherington@gmail.com>
2019-05-21 21:34:38 +0000

    downloaded and connected knex. uses knex callback function to query database

--- knex_script.js
+++ knex_script.js
@@ -1,29 +1,19 @@
-const knex = require("knex");
+const pg = require("pg");
 const settings = require("./settings");
 
-const client = new knex.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
-});
-
-let name =process.argv.slice(2)[0];
-
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
   }
-
- client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [name],
-    (err, result) => {
-    if (err) {
-    return console.error("error running query", err);
-    }
-    result.rows.forEach(row =>
-      console.log(row.first_name + " " + row.last_name + ", born " + row.birthdate.toLocaleDateString()));
-    client.end();
-  })
 });
+
+knex.select('first_name').from('famous_people')
+      .asCallback(function(err, rows) {
+        if (err) return console.error(err);
+        console.log("result ",rows);
+        knex.destroy();
+      });
