c78ae02a324f0b25889355c7da39a8bf0669b2ce
Liam Hetherington <liambhetherington@gmail.com>
2019-05-21 20:16:20 +0000

    create look_up people file that will do a query through famous_people database from a command line argument and find rows that match either first or last name in command line arguement

---
+++ lookup_people.js
@@ -0,0 +1,29 @@
+const pg = require("pg");
+const settings = require("./settings");
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+let name =process.argv.slice(2)[0];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+
+ client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [name],
+    (err, result) => {
+    if (err) {
+    return console.error("error running query", err);
+    }
+    result.rows.forEach(row =>
+      console.log(row.first_name + " " + row.last_name + ", born " + row.birthdate));
+    client.end();
+  })
+});
